6442abb398af7c4ee875ddca3d62f217a06667d0
Hagyeong Kim <78262460+hhagyeong@users.noreply.github.com>
2021-04-13 02:16:34 +0900

    Create: UK Commerce 데이터를 이용한 리포트 작성

---
+++ MySQL/UK_Commerce_REPORT.sql
@@ -0,0 +1,9 @@
+# UK Commenrce 데이터를 이용한 리포트 작성
+/* InvoiceNo: 주문 번호
+   StockCode: 상품 번호
+   Description: 상품명
+   Quantity: 구매 상품 수
+   UnitPrice: 개당 판매 가격
+   CustomerID: 고객 번호
+   Country: 판매 국가
+   InvoiceDate: 판매 일자 */
